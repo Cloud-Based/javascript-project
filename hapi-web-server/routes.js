7571d6bfb055319a3cdceb8f64546ead6bacbaec
TheeroDay <siteside.base@gmail.com>
2022-11-03 02:56:11 +0700

    Add new

--- hapi-web-server/routes.js
+++ hapi-web-server/routes.js
@@ -34,6 +34,19 @@ const routes = [
             return 'Halaman tidak ditemukan';
         },
     },
+    {
+    method: 'GET',
+    path: '/hello/{name?}',
+    handler: (request, h) => {
+      const { name = "stranger" } = request.params;
+      const { lang } = request.query;
+
+      if(lang === 'id') {
+          return `Hai, ${name}!`;
+      }
+      return `Hello, ${name}!`;
+   },
+ },
 ];
 
 module.exports = routes;
