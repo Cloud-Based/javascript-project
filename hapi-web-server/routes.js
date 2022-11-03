07d2cde303807f5a0ef19fcf4b2e564dbc9a33b9
TheeroDay <siteside.base@gmail.com>
2022-11-03 13:28:58 +0700

    Add new

--- hapi-web-server/routes.js
+++ hapi-web-server/routes.js
@@ -3,37 +3,44 @@ const routes = [
     method: 'GET',
     path: '/',
     handler: (request, h) => {
-      return 'Homepage';
+      return h.response('Homepage').code(200);
     },
   },
+
   {
         method: '*',
         path: '/',
         handler: (request, h) => {
-            return 'Halaman tidak dapat diakses dengan method tersebut';
+          const{ method } = request;
+          return h.response(`Halaman tidak dapat diakses dengan method ${ method }`).code(400);
         },
     },
+
   {
     method: 'GET',
     path: '/about',
     handler: (request, h) => {
-      return 'About Page';
+      return h.response('About Page').code(200);
     },
   },
+
   {
         method: '*',
         path: '/about',
         handler: (request, h) => {
-            return 'Halaman tidak dapat diakses dengan method';
+          const{ method } = request;
+          return h.response(`Halaman tidak dapat diakses dengan method ${ method }`).code(400);
         },
     },
+
     {
         method: '*',
         path: '/{any*}',
         handler: (request, h) => {
-            return 'Halaman tidak ditemukan';
+            return h.response('Halaman tidak ditemukan').code(404);
         },
     },
+
     {
     method: 'GET',
     path: '/hello/{name?}',
@@ -42,17 +49,18 @@ const routes = [
       const { lang } = request.query;
 
       if(lang === 'id') {
-          return `Hai, ${name}!`;
+          return h.response(`Hai, ${name}!`).code(200);
       }
-      return `Hello, ${name}!`;
+      return h.response(`Hello, ${name}!`).code(200);
    },
  },
+
  {
    method: 'POST',
    path: '/login',
    handler: (request, h) => {
       const { username, password } = request.payload;
-      return `Welcome ${username}!`;
+      return h.response(`Welcome ${username}!`).code(200);
     },
   },
 ];
